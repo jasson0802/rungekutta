464e0f4ec6d50cc15495d5c7ff1ea785bedc65dc
jgonzalezPernix <jgonzalez@pernixlabs.com>
2019-06-17 10:55:29 -0600

    Add variables

--- rungekutta.asm
+++ rungekutta.asm
@@ -28,8 +28,8 @@ _start:   mov       rax, 1                  ; system call for write
 		  ;ti = ti + h;
 		  ;yactual = yp;
 
-		interes:
-		
+		calcularganancia:
+		;D(t) = (i - p) D(t) + M
 		mov rax, 1
 		ret
 
@@ -37,15 +37,11 @@ _start:   mov       rax, 1                  ; system call for write
           section   .data
           
 message:  	db        "Hello, World", 10      ; note the newline at the end
-h:		  	dw 		0.5								; Salto
-iteraciones dw		10								;Cantidad de veces a calcular interes 		
+h:		  	dw 		1								; Salto
+iteraciones dw		10								;Cantidad de veces a calcular interes
+yactual:	dw		90								;Valor de D(0)
+t0:  		db 		0								;Variable independiente tiempo
 
-          section .bss
-		  korder1: resb 1
-		  korder2: resb 1
-		  korder3: resb 1
-		  korder4: resb 1
-		  yactual: resb 1
-		  x0:  resb 1
-
-;D(t) = (i - p) D(t) + M
+interes:	dw		0.04							; la tasa de interés estimada de inversión (por período)
+inflacion:	dw		0.07							; inflación en el período
+aporte:		dw		100								; el aporte adicional constante en cada trimestre
